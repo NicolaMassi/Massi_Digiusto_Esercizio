7e62b8ec362acb8abb454d9f259199502971dca1
Matteo Digiusto <matteo.digiusto@isitgo.it>
2022-11-23 10:10:38 +0100

    bug fix

--- Massi_Digiusto_Esercizio/src/Main.java
+++ Massi_Digiusto_Esercizio/src/Main.java
@@ -113,15 +113,15 @@ public class Main{
 
     }
 
-    public static void inserimentoAtleti(Scanner input, Atleta atleti[]){
+    public static void inserimentoAtleti(Scanner input, Atleta atleti[]) {
 
         String nomeAux, cognomeAux, specialitaAux;
         Data dataNascitaAux;
 
 
-        for(int i = 0; i < atleti.length; i++){
+        for (int i = 0; i < atleti.length; i++) {
 
-            System.out.print("Inserimento del "+ (i + 1) +"atleta: ");
+            System.out.print("Inserimento del " + (i + 1) + "atleta: ");
             System.out.print("Inserire il nome: ");
             nomeAux = input.nextLine();
             System.out.print("Inserire il cognome: ");
@@ -133,44 +133,7 @@ public class Main{
 
             atleti[i] = new Atleta(nomeAux, cognomeAux, dataNascitaAux, specialitaAux);
         }
-            public static void inserimentoGara(Scanner input, Gara gara, Atleta atleti[], int nAtleti){
-
-        Data dataAux;
-        int giornoAux;
-        String meseAux;
-        int annoAux;
-        int oraInizioAux;
-
-        System.out.println("Inserimento della data della gara");
-        dataAux = inserimentoData(input);
-
-        System.out.print("Inserire l'ora di inizio: ");
-        oraInizioAux = input.nextInt();
-
-        System.out.println("Inserimento degli atleti\n");
-        inserimentoAtleti(input, atleti);
 
     }
 
-    public static void inserimentoAtleti(Scanner input, Atleta atleti[]){
-
-        String nomeAux, cognomeAux, specialitaAux;
-        Data dataNascitaAux;
-
-
-        for(int i = 0; i < atleti.length; i++){
-
-            System.out.print("Inserimento del "+ (i + 1) +"atleta: ");
-            System.out.print("Inserire il nome: ");
-            nomeAux = input.nextLine();
-            System.out.print("Inserire il cognome: ");
-            cognomeAux = input.nextLine();
-            System.out.println("Inserimento della data di nascira dell'atleta");
-            dataNascitaAux = inserimentoData(input);
-            System.out.print("Inserire la specilità dell'atleta: ");
-            specialitaAux = input.nextLine();
-
-            atleti[i] = new Atleta(nomeAux, cognomeAux, dataNascitaAux, specialitaAux);
-        }
-    }
 }

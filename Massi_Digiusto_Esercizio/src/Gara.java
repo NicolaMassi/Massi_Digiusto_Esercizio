1fe9496131cd1aca3c4381ddbc1af69be80b30a5
Massi Nicola <nicola.massi@isitgo.it>
2022-11-23 09:07:13 +0100

    Implementazione classe Data e Gara

---
+++ Massi_Digiusto_Esercizio/src/Gara.java
@@ -0,0 +1,47 @@
+public class Gara {
+    private Data data;
+    private int oraInizio;
+    private Pista pisteDisp[] = new Pista[];
+    private Atleta atleti[] = new Atleta[];
+
+    public Gara(){}
+
+    public Gara(Data data, int oraInizio, Pista pisteDisp[], Atleta atleti[]){
+        this.data = data;
+        this.oraInizio = oraInizio;
+        this.pisteDisp[] = pisteDisp;
+        this.atleti[] = atleti;
+    }
+
+    public void setData(Data data){
+        this.data = data;
+    }
+
+    public void setOraInizio(int oraInizio){
+        this.oraInizio = oraInizio;
+    }
+
+    public void setPisteDisp(Pista pisteDisp[]){
+        this.pisteDisp = pisteDisp;
+    }
+
+    public void setAtleti(Atleta atleti[]){
+        this.atleti = atleti;
+    }
+
+    public Data getData(){
+        return data;
+    }
+
+    public int getOraInizio(){
+        return oraInizio;
+    }
+
+    public Pista getPisteDisp(){
+        return pisteDisp;
+    }
+
+    public Atleta getAtleti(){
+        return atleti;
+    }    
+}
